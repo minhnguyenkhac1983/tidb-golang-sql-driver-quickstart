614c4a10e498b863b43bdd5430c29b85480f6f16
Icemap <qizhi.wang@pingcap.com>
2023-08-25 12:18:29 +0800

    init: initial project

---
+++ dao.go
@@ -0,0 +1,261 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"math/rand"
+	"strings"
+
+	"github.com/google/uuid"
+)
+
+type Player struct {
+	ID    string
+	Coins int
+	Goods int
+}
+
+// recreateTable recreate table
+func recreateTable(db *sql.DB) {
+	if _, err := db.Exec(DropTableSQL); err != nil {
+		panic(err)
+	}
+	if _, err := db.Exec(CreateTableSQL); err != nil {
+		panic(err)
+	}
+}
+
+// createPlayer create a player
+func createPlayer(db *sql.DB, player Player) error {
+	_, err := db.Exec(CreatePlayerSQL, player.ID, player.Coins, player.Goods)
+	return err
+}
+
+// getPlayer get a player
+func getPlayer(db *sql.DB, id string) (Player, error) {
+	var player Player
+
+	rows, err := db.Query(GetPlayerSQL, id)
+	if err != nil {
+		return player, err
+	}
+	defer rows.Close()
+
+	if rows.Next() {
+		err = rows.Scan(&player.ID, &player.Coins, &player.Goods)
+		if err == nil {
+			return player, nil
+		} else {
+			return player, err
+		}
+	}
+
+	return player, fmt.Errorf("can not found player")
+}
+
+// getPlayerByLimit get players by limit
+func getPlayerByLimit(db *sql.DB, limit int) ([]Player, error) {
+	var players []Player
+
+	rows, err := db.Query(GetPlayerByLimitSQL, limit)
+	if err != nil {
+		return players, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		player := Player{}
+		err = rows.Scan(&player.ID, &player.Coins, &player.Goods)
+		if err == nil {
+			players = append(players, player)
+		} else {
+			return players, err
+		}
+	}
+
+	return players, nil
+}
+
+// bulk-insert players
+func bulkInsertPlayers(db *sql.DB, players []Player, batchSize int) error {
+	tx, err := TiDBSqlBegin(db, true)
+	if err != nil {
+		return err
+	}
+
+	stmt, err := tx.Prepare(buildBulkInsertSQL(batchSize))
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	for len(players) > batchSize {
+		if _, err := stmt.Exec(playerToArgs(players[:batchSize])...); err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		players = players[batchSize:]
+	}
+
+	if len(players) != 0 {
+		if _, err := tx.Exec(buildBulkInsertSQL(len(players)), playerToArgs(players)...); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+func getCount(db *sql.DB) (int, error) {
+	count := 0
+
+	rows, err := db.Query(GetCountSQL)
+	if err != nil {
+		return count, err
+	}
+
+	defer rows.Close()
+
+	if rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			return count, err
+		}
+	}
+
+	return count, nil
+}
+
+func tradeCheck(tx *TiDBSqlTx, sellID, buyID string, amount, price int) error {
+	var sellPlayer, buyPlayer Player
+	stmt, err := tx.Prepare(GetPlayerWithLockSQL)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	sellRows, err := stmt.Query(sellID)
+	if err != nil {
+		return err
+	}
+	defer sellRows.Close()
+
+	if sellRows.Next() {
+		if err := sellRows.Scan(&sellPlayer.ID, &sellPlayer.Coins, &sellPlayer.Goods); err != nil {
+			return err
+		}
+	}
+	sellRows.Close()
+
+	if sellPlayer.ID != sellID || sellPlayer.Goods < amount {
+		return fmt.Errorf("sell player %s goods not enough", sellID)
+	}
+
+	buyRows, err := stmt.Query(buyID)
+	if err != nil {
+		return err
+	}
+	defer buyRows.Close()
+
+	if buyRows.Next() {
+		if err := buyRows.Scan(&buyPlayer.ID, &buyPlayer.Coins, &buyPlayer.Goods); err != nil {
+			return err
+		}
+	}
+	buyRows.Close()
+
+	if buyPlayer.ID != buyID || buyPlayer.Coins < price {
+		return fmt.Errorf("buy player %s coins not enough", buyID)
+	}
+
+	return nil
+}
+
+func tradeUpdate(tx *TiDBSqlTx, sellID, buyID string, amount, price int) error {
+	updateStmt, err := tx.Prepare(UpdatePlayerSQL)
+	if err != nil {
+		return err
+	}
+	defer updateStmt.Close()
+
+	if _, err := updateStmt.Exec(-amount, price, sellID); err != nil {
+		return err
+	}
+
+	if _, err := updateStmt.Exec(amount, -price, buyID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func buyGoods(db *sql.DB, sellID, buyID string, amount, price int) error {
+	tx, err := TiDBSqlBegin(db, true)
+	if err != nil {
+		return err
+	}
+
+	buyExec := func() error {
+		if err := tradeCheck(tx, sellID, buyID, amount, price); err != nil {
+			return err
+		}
+
+		return tradeUpdate(tx, sellID, buyID, amount, price)
+	}
+
+	err = buyExec()
+	if err == nil {
+		fmt.Println("\n[buyGoods]:\n    'trade success'")
+		tx.Commit()
+	} else {
+		tx.Rollback()
+	}
+
+	return err
+}
+
+func playerToArgs(players []Player) []interface{} {
+	var args []interface{}
+	for _, player := range players {
+		args = append(args, player.ID, player.Coins, player.Goods)
+	}
+	return args
+}
+
+func buildBulkInsertSQL(amount int) string {
+	return CreatePlayerSQL + strings.Repeat(",(?,?,?)", amount-1)
+}
+
+func randomPlayers(amount int) []Player {
+	players := make([]Player, amount, amount)
+	for i := 0; i < amount; i++ {
+		players[i] = Player{
+			ID:    uuid.New().String(),
+			Coins: rand.Intn(10000),
+			Goods: rand.Intn(10000),
+		}
+	}
+
+	return players
+}
---
+++ sqldriver.go
@@ -0,0 +1,134 @@
+// Copyright 2022 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/joho/godotenv/autoload"
+)
+
+func main() {
+	// Configure the example database connection.
+	openDB("mysql", func(db *sql.DB) {
+		// 1. recreate the table.
+		recreateTable(db)
+
+		// 2. Run some simple examples.
+		simpleExample(db)
+
+		// 3. Getting further.
+		tradeExample(db)
+	})
+}
+
+func simpleExample(db *sql.DB) {
+	// Create a player, who has a coin and a goods.
+	err := createPlayer(db, Player{ID: "test", Coins: 1, Goods: 1})
+	if err != nil {
+		panic(err)
+	}
+
+	// Get a player.
+	testPlayer, err := getPlayer(db, "test")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("getPlayer: %+v\n", testPlayer)
+
+	// Create players with bulk inserts. Insert 1919 players totally, with 114 players per batch.
+
+	err = bulkInsertPlayers(db, randomPlayers(1919), 114)
+	if err != nil {
+		panic(err)
+	}
+
+	// Count players amount.
+	playersCount, err := getCount(db)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("countPlayers: %d\n", playersCount)
+
+	// Print 3 players.
+	threePlayers, err := getPlayerByLimit(db, 3)
+	if err != nil {
+		panic(err)
+	}
+	for index, player := range threePlayers {
+		fmt.Printf("print %d player: %+v\n", index+1, player)
+	}
+}
+
+func tradeExample(db *sql.DB) {
+	// Player 1: id is "1", has only 100 coins.
+	// Player 2: id is "2", has 114514 coins, and 20 goods.
+	player1 := Player{ID: "1", Coins: 100}
+	player2 := Player{ID: "2", Coins: 114514, Goods: 20}
+
+	// Create two players "by hand", using the INSERT statement on the backend.
+	if err := createPlayer(db, player1); err != nil {
+		panic(err)
+	}
+	if err := createPlayer(db, player2); err != nil {
+		panic(err)
+	}
+
+	// Player 1 wants to buy 10 goods from player 2.
+	// It will cost 500 coins, but player 1 cannot afford it.
+	fmt.Println("\nbuyGoods:\n    => this trade will fail")
+	if err := buyGoods(db, player2.ID, player1.ID, 10, 500); err == nil {
+		panic("there shouldn't be success")
+	}
+
+	// So player 1 has to reduce the incoming quantity to two.
+	fmt.Println("\nbuyGoods:\n    => this trade will success")
+	if err := buyGoods(db, player2.ID, player1.ID, 2, 100); err != nil {
+		panic(err)
+	}
+}
+
+func openDB(driverName string, runnable func(db *sql.DB)) {
+	db, err := sql.Open(driverName, getDSN())
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	runnable(db)
+}
+
+func getDSN() string {
+	tidbHost := getEnvWithDefault("TIDB_HOST", "127.0.0.1")
+	tidbPort := getEnvWithDefault("TIDB_PORT", "4000")
+	tidbUser := getEnvWithDefault("TIDB_USER", "root")
+	tidbPassword := getEnvWithDefault("TIDB_PASSWORD", "")
+	tidbDBName := getEnvWithDefault("TIDB_DB_NAME", "test")
+	useSSL := getEnvWithDefault("USE_SSL", "false")
+
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&tls=%s",
+		tidbUser, tidbPassword, tidbHost, tidbPort, tidbDBName, useSSL)
+}
+
+func getEnvWithDefault(key, fallback string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return fallback
+	}
+	return value
+}
